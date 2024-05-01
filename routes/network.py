162b430e2d51d74445258544966e87507d24eaca
Natdanai Pinaves <jgogo012@gmail.com>
2024-05-02 04:40:39 +0700

    Refactor: IP & DNS Group Varable

--- routes/network.py
+++ routes/network.py
@@ -12,29 +12,28 @@ async def metrics(target: str):
     try:
         #Get network
         requestNetwork = requests.get(f"http://{target}/network").json()
+        requestGeneral = requests.get(f"http://{target}/general").json()
         network = NetworkResult(requestNetwork["status"], requestNetwork["message"], requestNetwork["data"])
+        general = GeneralResult(requestGeneral["status"], requestGeneral["message"], requestGeneral["data"])
         
         #Set Prometheus metrics
         ## DNS
         DNS_STATUS.info({
+            "indentity": general.data.identity,
             "dns_status": "True" if network.data.lan.dns.status and
             network.data.wlan.dns.status else "False",
             "lan_dns": "True" if network.data.lan.dns.status else "False",
-            "wlan_dns": "True" if network.data.wlan.dns.status else "False"
-        })
-        DNS_RESPONSE_TIME.info({
+            "wlan_dns": "True" if network.data.wlan.dns.status else "False",
             "lan_dns_response_time": f"{network.data.lan.dns.response_time}",
             "wlan_dns_response_time": f"{network.data.wlan.dns.response_time}"
         })
-        
+
         ## IP
-        IP_ADDRESS.info({
+        IP_STATUS.info({
             "lan_ipv4": network.data.lan.ipv4 if network.data.lan.ipv4 != None else "",
             "lan_ipv6": network.data.lan.ipv6 if network.data.lan.ipv6 != None else "",
             "wlan_ipv4": network.data.wlan.ipv4 if network.data.wlan.ipv4 != None else "",
             "wlan_ipv6": network.data.wlan.ipv6 if network.data.wlan.ipv6 != None else "",
-        })
-        IP_STATUS.info({
             "lan_ipv4": "1" if network.data.lan.ipv4 != None else "0",
             "lan_ipv6": "1" if network.data.lan.ipv6 != None else "0",
             "wlan_ipv4": "1" if network.data.wlan.ipv4 != None else "0",
--- utils/network.py
+++ utils/network.py
@@ -6,8 +6,6 @@ network_registry = CollectorRegistry()
 
 # DNS Metrics
 DNS_STATUS = prom.Info("DNS_STATUS", "DNS Resovler Status", registry=network_registry)
-DNS_RESPONSE_TIME = prom.Info("DNS_RESPONSE_TIME", "DNS Resovler Response Time", registry=network_registry)
 
 # IP Metrics
 IP_ADDRESS = prom.Info("IP_ADDRESS", "IP Address", registry=network_registry)
-IP_STATUS = prom.Info("IP_STATUS", "IP Address Status", registry=network_registry)
