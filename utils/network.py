e2a3f9d2c77b985bcb555350afe1c81ca877e032
Natdanai Pinaves <jgogo012@gmail.com>
2024-05-02 03:12:20 +0700

    Fix: Missing Registry

--- utils/network.py
+++ utils/network.py
@@ -5,9 +5,9 @@ from prometheus_client import CollectorRegistry
 network_registry = CollectorRegistry()
 
 # DNS Metrics
-DNS_STATUS = prom.Info("DNS_STATUS", "DNS Resovler Status")
-DNS_RESPONSE_TIME = prom.Info("DNS_RESPONSE_TIME", "DNS Resovler Response Time")
+DNS_STATUS = prom.Info("DNS_STATUS", "DNS Resovler Status", registry=network_registry)
+DNS_RESPONSE_TIME = prom.Info("DNS_RESPONSE_TIME", "DNS Resovler Response Time", registry=network_registry)
 
 # IP Metrics
-IP_ADDRESS = prom.Info("IP_ADDRESS", "IP Address")
-IP_STATUS = prom.Info("IP_STATUS", "IP Address Status")
+IP_ADDRESS = prom.Info("IP_ADDRESS", "IP Address", registry=network_registry)
+IP_STATUS = prom.Info("IP_STATUS", "IP Address Status", registry=network_registry)
